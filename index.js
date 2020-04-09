b825e593a1f176772011c4f831aac56c5124c968
DmytroKulish <62489472+DmytroKulish@users.noreply.github.com>
2020-04-09 15:05:44 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ for (var i = 0; i < numberOfDrums; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
-
+    buttonAnimation(buttonInnerHTML)
   })
 }
 
@@ -14,6 +14,7 @@ for (var i = 0; i < numberOfDrums; i++) {
 document.addEventListener("keydown", function(event) {
   var key = event.key;
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 
@@ -58,5 +59,22 @@ function makeSound(key) {
 
   }
 
+
+
+}
+
+
+
+
+
+
+
+
+
+
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+ setTimeout(function(){ activeButton.classList.remove("pressed"); }, 100);
 
 }
